47b61fd0c0e35099c6ac1b0069eb8a19a89bd616
Shane Barringer <shane.barringer@outlook.com>
2019-01-26 19:30:24 -0600

    adds combine tests

--- katas/00-combine.js
+++ katas/00-combine.js
@@ -1,7 +1,7 @@
 // Write a function named combine() that takes two arguments and returns a single array
 
-const combine = (first, ...rest) => {
-  return [first, ...rest].reduce((acc, curr) => acc.concat(curr), []);
+const combine = () => {
+  // solution here
 };
 
 module.exports = combine;
