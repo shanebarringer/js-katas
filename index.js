b43c31292b0e9d07e20972da31b8280136c6bd0c
Shane Barringer <shane.barringer@outlook.com>
2019-01-26 21:17:32 -0600

    updates instructions in index

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 console.log(
-  "Starting with 01-katas/last.js, run the test suite with 'npm run watch' and write the passing code"
+  "Starting with 00-katas/convertToNumbers.js, run the test suite with 'npm run watch' and write the passing code"
 );
