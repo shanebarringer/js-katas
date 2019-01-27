768e456ae6c13f4e5913771790cde7a23dd0c96d
Shane Barringer <shane.barringer@outlook.com>
2019-01-26 19:05:51 -0600

    adds last.js and associated tests

---
+++ __tests__/last.test.js
@@ -0,0 +1,10 @@
+const last = require("../last");
+
+describe("last() returns the last value", () => {
+  test("given the array: [2, 4, 6, 8]) the return value is 8", () => {
+    expect(last([2, 4, 6, 8])).toEqual(8);
+  });
+  test("given the values: 1, 22, 343, 42, 55, 77) the return value is 77", () => {
+    expect(last(1, 22, 343, 42, 55, 77)).toEqual(77);
+  });
+});
---
+++ index.js
@@ -0,0 +1,3 @@
+console.log(
+  "Starting with last.js, run the test suite with 'npm run watch' and write the passing code"
+);
